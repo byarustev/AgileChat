7e09be8750d15cebed5a651472db2bda81c4c29f
Isaac Owomugisha <archimedesisaac@gmail.com>
2018-07-02 11:27:06 +0300

    Add login and create account methods

--- user.py
+++ user.py
@@ -1,3 +1,6 @@
+from AgileChat import users
+
+
 class User:
     def __init__(self, _id, username, name, password):
         self.user_id = _id
@@ -5,8 +8,24 @@ class User:
         self.name = name
         self.password = password
 
-    def login(self, username, password):
-        pass
+    @staticmethod
+    def login(username, password):
+        for user in users:
+            if user.username == username and user.password == password:
+                return True
+
+        return False
+
+    @staticmethod
+    def create_account(username, name, password):
+        for user in users:
+            if user.username == username or user.name == name:
+                print("User already exists")
+                return False
+
+        user = User(len(users), username, name, password)
+        users.append(user)
+        return True
 
     def create_comment(self, comment):
         pass
