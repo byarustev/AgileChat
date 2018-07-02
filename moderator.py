a409881ad8f67dd1d1d9bff161c9ea3bce79d69f
maria nanfuka <mariah.kakande@gmail.com>
2018-07-02 11:53:50 +0300

    Edit moderator method

--- moderator.py
+++ moderator.py
@@ -1,4 +1,8 @@
-class moderators:
+from comment import Comment
+from user import User
+from AgileChat import comments
+
+class Moderators(User):
     def __init__(self,moderator_id,moderator_name):
         self.moderator_id = moderator_id
         self.moderator_name = moderator_name
@@ -7,12 +11,13 @@ class moderators:
     def edit_comments(self,):
         pass
      
-      
-        
+             
         
          
+      
+    def delete_comment(self,comment_id):
+        comments.remove(comment_id - 1)
         
-    def delete_comment(self):
-        pass
+        
 
        
